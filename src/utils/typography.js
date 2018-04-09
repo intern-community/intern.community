7cfabc80edbbc18e72059fb139976221560f171b
greg-wolff <gregwwolff@gmail.com>
2018-04-08 19:04:07 -0700

    added typography.js

---
+++ src/utils/typography.js
@@ -0,0 +1,12 @@
+import Typography from "typography";
+
+const typography = new Typography({ 
+  title: 'Intern Community',
+  baseFontSize: "19px",
+  baseLineHeight: 1.58,
+  headerFontFamily: ['C', 'sans-serif'],
+  bodyFontFamily: ['C', 'serif'],
+
+});
+
+export default typography;
