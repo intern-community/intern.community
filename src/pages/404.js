082d342dd5eaf3cf265b6ad097ff257c236f6798
greg-wolff <gregwwolff@gmail.com>
2018-04-11 19:31:20 -0700

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+import TextHero from '../components/TextHero'
+
+const Image = styled.img`
+  border-radius: 5px;
+  box-shadow: 0 5px 20px rgba(58,71,80,.2);
+`
 
 const NotFoundPage = () => (
   <div>
-    <h1>404 NOT FOUND</h1>
+    <TextHero centered>
+      <h1>404 NOT FOUND</h1>
+      <Image src="https://i.redd.it/ni7xbwxvdbh01.gif"/>
+    </TextHero>
   </div>
 )
 
