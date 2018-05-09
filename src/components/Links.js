768d0d2d03512283f56ff7f9e480f8b8733bfc00
greg-wolff <gregwwolff@gmail.com>
2018-05-09 16:59:25 -0700

    added fancy form submission button

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import Link from 'gatsby-link';
-import { lighten, desaturate } from 'polished';
+import { darken, desaturate, lighten } from 'polished';
 
 const defaultTheme = {
   theme: {
@@ -38,6 +38,7 @@ export const StyledAnchor = StyledLink.withComponent('a');
 export const ArrowLink = StyledLink.extend`
   position: relative;
   margin: 0 24px 0 0;
+  transition: opacity 0.5s ease;
   &:after {
     content: " →";
     right: -24px;
@@ -52,3 +53,43 @@ export const ArrowLink = StyledLink.extend`
 
 export const ArrowButton = ArrowLink.withComponent('button');
 export const ArrowAnchor = StyledLink.withComponent('a');
+
+export const CtaButton = styled.button`
+  margin-top: 0.4em;
+  width: 100%;
+  border: 1px solid ${props => props.theme.main};
+  padding: 5px 15px;
+  border-radius: 5px;
+  font-weight: bold;
+  padding: 5px 15px 7px 15px;
+  color: ${props => props.theme.main};
+  box-shadow: 0px 0px 0 ${props => props.theme.main};
+  transition: all 0.2s ease;
+  position: relative;
+  text-transform: uppercase;
+  top: 0;
+  left: 0;
+  &:hover,
+  &:focus {
+    top: -2px;
+    left: -2px;
+    color: ${props => lighten(0.1, props.theme.main)};
+    box-shadow: 2px 2px 0 ${props => lighten(0.1, props.theme.main)};
+    border: 1px solid ${props => lighten(0.1, props.theme.main)}
+  }
+  &:active {
+    top: 0;
+    left: 0;
+    color: ${props => props.theme.main};
+    border: 1px solid ${props => props.theme.main};
+    box-shadow: 0 0 0 ${props => props.theme.main};
+  }
+  &:focus {
+    outline: none;
+  }
+  &:disabled {
+    color: ${props => desaturate(.5, props.theme.main)};
+  }
+`;
+
+CtaButton.defaultProps = defaultTheme;
